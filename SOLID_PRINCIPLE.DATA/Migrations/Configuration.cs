73c4725de6c00ffd05d1048a3412c8cffdf9ab70
kietnm <kietnm2505@gmail.com>
2017-12-11 13:13:53 +0700

    OK

---
+++ SOLID_PRINCIPLE.DATA/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace SOLID_PRINCIPLE.DATA.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<SOLID_PRINCIPLE.DATA.StoreEntities>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(SOLID_PRINCIPLE.DATA.StoreEntities context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
--- SOLID_PRINCIPLE.WEB/Global.asax.cs
+++ SOLID_PRINCIPLE.WEB/Global.asax.cs
@@ -7,10 +7,7 @@ namespace SOLID_PRINCIPLE.WEB
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
-        {
-            // Init database
-            // System.Data.Entity.Database.SetInitializer((new DbSeed());
-
+        {        
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
